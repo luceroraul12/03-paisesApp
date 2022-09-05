cf1d9d35e0c9f726a581af17080723cfcff996a0
Lucero Raul <luceroraul12@gmail.com>
2022-09-05 20:41:49 -0300

    add componentes

---
+++ src/app/pais/pais.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { PorCapitalComponent } from './pages/por-capital/por-capital.component';
+import { PorPaisComponent } from './pages/por-pais/por-pais.component';
+import { VerPaisComponent } from './pages/ver-pais/ver-pais.component';
+
+
+
+@NgModule({
+  declarations: [
+    PorCapitalComponent,
+    PorPaisComponent,
+    VerPaisComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class PaisModule { }

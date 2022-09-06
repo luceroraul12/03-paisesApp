ccf4d40412b96cd3a44b6b0f7833ffd8d81a34be
Lucero Raul <luceroraul12@gmail.com>
2022-09-05 23:15:41 -0300

    add metodo get paises

--- src/app/services/pais.service.ts
+++ src/app/services/pais.service.ts
@@ -1,9 +1,21 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PaisService {
 
-  constructor() { }
+  urlApi: string = "https://restcountries.com/v2";
+
+  constructor(private http: HttpClient) { }
+
+  buscarPorPais(busqueda: string): Observable<any>{
+    const url: string = `${this.urlApi}/name/${busqueda}`;
+
+    return this.http.get(url);
+  }
+  
+
 }

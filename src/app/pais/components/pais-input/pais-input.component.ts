e841383c20730af4a0933925bfb7bf7df226d42b
Lucero Raul <luceroraul12@gmail.com>
2022-09-06 14:25:00 -0300

    conexión input event

--- src/app/pais/components/pais-input/pais-input.component.ts
+++ src/app/pais/components/pais-input/pais-input.component.ts
@@ -1,25 +1,34 @@
 import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { Subject } from 'rxjs';
 
 @Component({
   selector: 'app-pais-input',
   templateUrl: './pais-input.component.html',
-  styleUrls: ['./pais-input.component.css']
+  styleUrls: ['./pais-input.component.css'],
 })
 export class PaisInputComponent implements OnInit {
-
-  public termino: string = "";
-
   @Output() onEnter: EventEmitter<string> = new EventEmitter();
+  @Output() onDebounce: EventEmitter<string> = new EventEmitter();
 
-  constructor() { }
+  public termino: string = '';
+
+  debouncer: Subject<string> = new Subject();
+
+  constructor() {
+    console.log('Constructor de input');
+  }
 
   ngOnInit(): void {
+    this.debouncer.subscribe((valor) => console.log('debouncer: ', valor));
   }
 
-  buscar(): void{
+  buscar(): void {
     console.log(this.termino);
     this.onEnter.emit(this.termino);
-    this.termino = "";
+    this.termino = '';
   }
 
+  teclaPresionada() {
+    this.debouncer.next(this.termino);
+  }
 }

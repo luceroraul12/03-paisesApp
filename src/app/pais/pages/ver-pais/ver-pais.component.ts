2634a2f3d6ef8796f2af7338579471d74441406e
Lucero Raul <luceroraul12@gmail.com>
2022-09-06 16:35:07 -0300

    observables anidados

--- src/app/pais/pages/ver-pais/ver-pais.component.ts
+++ src/app/pais/pages/ver-pais/ver-pais.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { PaisService } from '../../../services/pais.service';
 
 @Component({
   selector: 'app-ver-pais',
@@ -7,11 +8,18 @@ import { ActivatedRoute } from '@angular/router';
   styleUrls: ['./ver-pais.component.css'],
 })
 export class VerPaisComponent implements OnInit {
-  constructor(private activateRoute: ActivatedRoute) {}
+  constructor(
+    private activateRoute: ActivatedRoute,
+    private paisService: PaisService
+  ) {}
 
   ngOnInit(): void {
-    this.activateRoute.params.subscribe((params) => {
-      console.log(params);
+    this.activateRoute.params.subscribe(({ id }) => {
+      console.log(id);
+
+      this.paisService
+        .buscarPorCodigoAlpha(id)
+        .subscribe((resultado) => console.log(resultado));
     });
   }
 }

909c604e28b075f33e4e1e9de86e6d94e8cd33d7
Lucero Raul <luceroraul12@gmail.com>
2022-09-06 13:37:14 -0300

    add pais-tabla-component

--- src/app/pais/pais.module.ts
+++ src/app/pais/pais.module.ts
@@ -5,6 +5,7 @@ import { PorPaisComponent } from './pages/por-pais/por-pais.component';
 import { VerPaisComponent } from './pages/ver-pais/ver-pais.component';
 import { PorRegionComponent } from './pages/por-region/por-region.component';
 import { FormsModule } from '@angular/forms';
+import { PaisTablaComponent } from './components/pais-tabla/pais-tabla.component';
 
 
 
@@ -13,7 +14,8 @@ import { FormsModule } from '@angular/forms';
     PorCapitalComponent,
     PorPaisComponent,
     VerPaisComponent,
-    PorRegionComponent
+    PorRegionComponent,
+    PaisTablaComponent
   ],
   imports: [
     CommonModule,

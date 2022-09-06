16ab85cd52f374e1340bdf4ca9b51bf66dab4020
Lucero Raul <luceroraul12@gmail.com>
2022-09-06 16:30:56 -0300

    obteniendo :id de url

--- src/app/pais/pages/ver-pais/ver-pais.component.ts
+++ src/app/pais/pages/ver-pais/ver-pais.component.ts
@@ -1,15 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-ver-pais',
   templateUrl: './ver-pais.component.html',
-  styleUrls: ['./ver-pais.component.css']
+  styleUrls: ['./ver-pais.component.css'],
 })
 export class VerPaisComponent implements OnInit {
-
-  constructor() { }
+  constructor(private activateRoute: ActivatedRoute) {}
 
   ngOnInit(): void {
+    this.activateRoute.params.subscribe((params) => {
+      console.log(params);
+    });
   }
-
 }

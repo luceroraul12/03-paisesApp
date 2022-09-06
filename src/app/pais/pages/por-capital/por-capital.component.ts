fe7b8b31459b2f4f9ed1f88aad0788ac6573c938
Lucero Raul <luceroraul12@gmail.com>
2022-09-06 14:49:50 -0300

    add funcionalidad por capital component

--- src/app/pais/pages/por-capital/por-capital.component.ts
+++ src/app/pais/pages/por-capital/por-capital.component.ts
@@ -1,15 +1,42 @@
 import { Component, OnInit } from '@angular/core';
+import { Pais } from '../../interface/pais.interface';
+import { PaisService } from '../../../services/pais.service';
 
 @Component({
   selector: 'app-por-capital',
   templateUrl: './por-capital.component.html',
-  styleUrls: ['./por-capital.component.css']
+  styleUrls: ['./por-capital.component.css'],
 })
 export class PorCapitalComponent implements OnInit {
+  termino: string = '';
+  hayError: boolean = false;
+  paisesEncontrados: Pais[] = [];
 
-  constructor() { }
+  constructor(private paisService: PaisService) {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  buscar(termino: string): void {
+    this.hayError = false;
+    this.termino = termino;
+    if (this.termino == '') {
+      return;
+    }
+    this.paisService.buscarPorCapital(this.termino).subscribe(
+      (respuesta) => {
+        console.log(respuesta);
+        this.paisesEncontrados = respuesta;
+        this.hayError = !respuesta;
+      },
+      (error) => {
+        console.log(error);
+        this.hayError = true;
+      }
+    );
+    this.termino = '';
   }
 
+  obtenerSugerencia(termino: string) {
+    this.hayError = false;
+  }
 }
--- src/app/services/pais.service.ts
+++ src/app/services/pais.service.ts
@@ -4,19 +4,21 @@ import { Observable } from 'rxjs';
 import { Pais } from '../pais/interface/pais.interface';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class PaisService {
+  urlApi: string = 'https://restcountries.com/v2';
 
-  urlApi: string = "https://restcountries.com/v2";
+  constructor(private http: HttpClient) {}
 
-  constructor(private http: HttpClient) { }
-
-  buscarPorPais(busqueda: string): Observable<Pais[]>{
+  buscarPorPais(busqueda: string): Observable<Pais[]> {
     const url: string = `${this.urlApi}/name/${busqueda}`;
 
     return this.http.get<Pais[]>(url);
   }
-  
 
+  buscarPorCapital(busqueda: string): Observable<Pais[]> {
+    const url: string = `${this.urlApi}/capital/${busqueda}`;
+    return this.http.get<Pais[]>(url);
+  }
 }
